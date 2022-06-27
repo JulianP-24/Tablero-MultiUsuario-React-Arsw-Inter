6caab03203e42ccacbbcc1f71d780390ec065ac6
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-06-27 11:10:17 -0500

    Primera Version

---
+++ src/test/java/edu/escuelaing/arsw/AppTest.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.arsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
